280c1321e7c0fbb17a3624dd6c409e54e7caad94
Vanessa Virgitti <virgitti@gmail.com>
2015-07-09 18:49:50 +0100

    added localstorage but the routing fails

--- server.js
+++ server.js
@@ -6,25 +6,28 @@ mongoose.connect('mongodb://DrStrange:MarvelRocks@ds053312.mongolab.com:53312/su
 var Superhero = require('./app/models/superhero');
 
 // app.use(express.static(__dirname, '/public'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.set('/public', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/superhero', function(req, res) {
   var superhero_name = req.body.superheroName;
   console.log("Here is your superhero name: " + superhero_name);
-  res.end("yes");
+  // res.end("yes");
+  res.redirect(__dirname + "/superhero.html");
   // res.sendFile(__dirname + '/public/index.html')
 });
 
-// app.get('/superhero', function(req, res) {
-//   res.sendFile(__dirname + '/public/index.html');
-// });
+app.get('/superhero', function(req, res) {
+  res.sendFile(__dirname + '/superhero.html');
+});
 
 var port = process.env.PORT || 3000;
 
