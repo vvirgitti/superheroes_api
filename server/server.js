654b485270f7c61cc37080daf1ae6fee702fa5af
Vanessa Virgitti <virgitti@gmail.com>
2015-07-10 14:58:08 +0100

    post request workinggit add .

--- server/server.js
+++ server/server.js
@@ -1,29 +1,28 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var path = require("path");
 
-// app.use(express.static(__dirname, '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/views'));
+
+var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('index');
 });
 
-app.post('/superhero', function(req, res) {
+app.post('/superhero', function(req, res){
   var superhero_name = req.body.superheroName;
-  console.log("Here is your superhero name: " + superhero_name);
-  // res.end("yes");
-  res.redirect(__dirname + "/superhero.html");
-  // res.sendFile(__dirname + '/public/index.html')
-});
+  console.log('Here is your superhero name: ' + superhero_name);
+  res.redirect('http://localhost:3000/superhero.html');
+  })
 
 app.get('/superhero', function(req, res) {
-  res.sendFile(__dirname + '/superhero.html');
+  res.sendFile(__dirname + 'superhero');
 });
 
 var port = process.env.PORT || 3000;
