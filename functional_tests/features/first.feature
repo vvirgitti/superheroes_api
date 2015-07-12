21872c50a919f6fbf28973b1b5b6401b9c181133
Vanessa <virgitti@gmail.com>
2015-07-12 19:20:39 +0100

    added first acceptance test and first unit jasmine test

---
+++ functional_tests/features/first.feature
@@ -0,0 +1,6 @@
+Feature: First feature to test
+
+  Scenario: Test
+  Given I'm on the homepage
+  When I enter the name of a superhero
+  Then I see the image of that superhero
---
+++ functional_tests/features/step_definitions/first.rb
@@ -0,0 +1,11 @@
+Given(/^I'm on the homepage$/) do
+  visit '/'
+end
+
+When(/^I enter the name of a superhero$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see the image of that superhero$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
